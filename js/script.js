0f6e77b8219e2e7cfad6d36d8e04ba8f6088eacf
ZARE ALIREZA <alireza.zare@etu.unistra.fr>
2023-12-18 08:14:40 +0100

    add option to create group with cards

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let team = [];
+
 document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementById("search-button");
     const searchBox = document.getElementById("search-box");
@@ -5,19 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
     searchButton.addEventListener("click", () => {
         const searchTerm = searchBox.value.trim();
         if (searchTerm) {
-            searchPokemon(searchTerm);
+            fetchAndDisplayPokemon(searchTerm);
         }
+    });
+
+    document.getElementById("clear-team").addEventListener("click", () => {
+        team = [];
+        updateTeamDisplay();
     });
 });
 
-function searchPokemon(searchTerm) {
+function fetchAndDisplayPokemon(pokemonName) {
     const pokemonDisplay = document.getElementById("pokemon-display");
-    pokemonDisplay.innerHTML = ''; 
+    pokemonDisplay.innerHTML = ''; // Clear previous results
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${searchTerm.toLowerCase()}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
         .then(response => {
             if (!response.ok) {
-                throw new Error('Pokémon not found'); // Throw an error if response is not OK
+                throw new Error('Pokémon not found');
             }
             return response.json();
         })
@@ -25,12 +32,11 @@ function searchPokemon(searchTerm) {
             displayPokemon(data);
         })
         .catch(error => {
-            pokemonDisplay.innerHTML = `<p class="error-message">${error.message}</p>`; // Display error message
+            pokemonDisplay.innerHTML = `<p class="error-message">${error.message}</p>`;
         });
 }
 
 function displayPokemon(pokemon) {
-    console.log(pokemon);
     const pokemonDisplay = document.getElementById("pokemon-display");
     pokemonDisplay.innerHTML = `
         <div class="pokemon-card">
@@ -38,6 +44,26 @@ function displayPokemon(pokemon) {
             <h2>${pokemon.name}</h2>
             <p>Attack: ${pokemon.stats[1].base_stat}</p>
             <p>Defense: ${pokemon.stats[2].base_stat}</p>
+            <button onclick="addToTeam('${pokemon.name}', '${pokemon.sprites.front_default}')">Add to Team</button>
         </div>
     `;
+}
+
+function addToTeam(pokemonName, imageUrl) {
+    if (team.length < 6 && !team.some(member => member.name === pokemonName)) {
+        team.push({ name: pokemonName, image: imageUrl });
+        updateTeamDisplay();
+    } else {
+        alert("Your team is already full or the Pokémon is already in your team.");
+    }
+}
+
+function updateTeamDisplay() {
+    const teamDisplay = document.getElementById("pokemon-team");
+    teamDisplay.innerHTML = team.map(member => `
+        <div class="team-member-card">
+            <img src="${member.image}" alt="${member.name}" class="team-member-image"/>
+            <p>${member.name}</p>
+        </div>
+    `).join('');
 }
