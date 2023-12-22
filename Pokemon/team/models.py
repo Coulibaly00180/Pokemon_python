04145ee86286182ef8912d50ee87d39de1a15be4
Coulibaly Christian <coulibalychristian00180@gmail.com>
2023-12-22 15:37:58 +0100

    Ajout des fonctions à faire dans team

--- Pokemon/team/models.py
+++ Pokemon/team/models.py
@@ -1,12 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import User
 from pokedex.models import Pokemon
 
 # Create your models here.
 class Team(models.Model):
     name = models.CharField(max_length=30)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     pokemons = models.ManyToManyField(Pokemon)
 
     def __str__(self):
         return self.name
+
+    def createTeam(name_team):
+        pass
+
+    def deleteTeam(name_team):
+        pass
+
+    def renameTeam(name_team):
+        pass
+
+    def addPokemonTeam(pokemon):
+        pass
+
+    def clearAllPokemonTeam(name_team):
+        pass
